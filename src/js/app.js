d2bfa8b9a509d72f2aed5210de4313d43aa89281
jmhead <jmhead@jasonmhead.com>
2015-07-05 18:12:01 -0700

    dev

--- src/js/app.js
+++ src/js/app.js
@@ -106,7 +106,7 @@ $(function() {
         //var arr = ['key-right', 'key-left', 'key-down', 'key-up'];
         // moved to global var
         
-        var direction = arr[Math.floor(Math.random()*current.keys)];
+        var direction = arr[Math.round(Math.random()*current.keys)];
         console.log(Math.floor(Math.random()*current.keys)); // test line
         var $elem = $container.find('.idle').first();
         if ($elem.length <= 0) {
