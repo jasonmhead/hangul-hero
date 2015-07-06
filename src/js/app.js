111eee6a1ea8f57adaad9ca8f6e8f54995be5973
jmhead <jmhead@jasonmhead.com>
2015-07-05 17:44:25 -0700

    dev

--- src/js/app.js
+++ src/js/app.js
@@ -109,7 +109,7 @@ $(function() {
         var direction = arr[Math.floor(Math.random()*current.keys)];
         var $elem = $container.find('.idle').first();
         if ($elem.length <= 0) {
-            $elem = $('<div class="key key-up'+direction+'"></div>');
+            $elem = $('<div class="key key-up s-key-up '+direction+'"></div>');
             $elem.on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
 
                 if (started === 'end' || started === 'restart' || $(this).hasClass('idle')) {
